d7024a2bd1c6e73128cfe18f5e15bdc837fa5915
sreenivas -unbxd <sreenivas.naik@unbxd.com>
2024-12-05 15:22:24 +0530

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -12,9 +12,9 @@ terraform {
 }
 
 # AWS Provider Configuration
-provider "aws" {
-  region  = "ap-south-1"
-  profile = "sreenivas"
+#provider "aws" {
+  #region  = "ap-south-1"
+  #profile = "sreenivas"
 }
 
  #Docker Provider Configuration
